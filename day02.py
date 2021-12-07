859027ace6ca807db11583c976745c161d9613f9
James Langley <jwblangley@gmail.com>
2021-12-07 12:27:35 +0000

    Implement parse_instructions

--- day02.py
+++ day02.py
@@ -3,8 +3,18 @@ import collections
 Location = collections.namedtuple("Location", ["displacement", "depth"])
 
 
-def parse_instructions(ins):
+def parse_instructions(instructions):
     displacement = 0
     depth = 0
+    for ins in instructions:
+        command, value = ins.split(" ")
+        if command == "forward":
+            displacement += int(value)
+        elif command == "down":
+            depth += int(value)
+        elif command == "up":
+            depth -= int(value)
+        else:
+            raise RuntimeError(f"Invalid command: {command}")
 
     return Location(displacement, depth)
