615bfc36bba586a14b3b2499106e2f28bcde6d95
James Langley <jwblangley@gmail.com>
2021-12-08 12:45:19 +0000

    Implement day03

--- day03.py
+++ day03.py
@@ -1,2 +1,37 @@
+import numpy as np
+
+
+def numpy_bin_to_uint(np_bin):
+    bits = len(np_bin)
+    acc = 0
+
+    for i in range(bits):
+        power = bits - 1 - i
+        acc += np_bin[i] << power
+
+    return acc
+
+
 def gamma_epsilon_from_bin(bin):
-    return 1, 1
+    totals = None
+    count = 0
+
+    for line in bin:
+        if totals is None:
+            totals = np.array([0] * len(line))
+
+        if len(line) != len(totals):
+            raise RuntimeError("Mismatched binary lengths")
+
+        temp = np.array([int(c) for c in line])
+        totals += temp
+        count += 1
+
+    avgs = totals / count
+    gamma = (avgs >= 0.5).astype(int)
+    epsilon = (avgs <= 0.5).astype(int)
+
+    gamma = numpy_bin_to_uint(gamma)
+    epsilon = numpy_bin_to_uint(epsilon)
+
+    return gamma, epsilon
--- test_day03.py
+++ test_day03.py
@@ -1,6 +1,8 @@
 import pytest
 
-from day03 import gamma_epsilon_from_bin
+import numpy as np
+
+from day03 import gamma_epsilon_from_bin, numpy_bin_to_uint
 
 
 @pytest.mark.parametrize(
@@ -67,3 +69,30 @@ def test_gamma_episolon_from_bin_mismatched_lengths(bin):
     with pytest.raises(RuntimeError):
         # WHEN
         gamma_epsilon_from_bin(bin)
+
+
+@pytest.mark.parametrize(
+    "np_bin,exp",
+    [
+        (np.array([1]), 1),
+        (np.array([0]), 0),
+        (np.array([1, 0]), 2),
+        (np.array([1, 1]), 3),
+        (np.array([0, 1]), 1),
+        (np.array([0, 0, 0, 0, 1]), 1),
+        (np.array([1, 0, 1, 0, 1]), 21),
+        (np.array([1, 1, 0, 0, 1]), 25),
+    ],
+)
+def test_numpy_bin_to_uint(np_bin, exp):
+    # GIVEN
+    """
+    input: np_bin
+    expectetd output: exp
+    """
+
+    # WHEN
+    res = numpy_bin_to_uint(np_bin)
+
+    # THEN
+    assert res == exp
