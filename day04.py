c3f9d56775f17a6e2e81639f39ebcae6ee90a119
James Langley <jwblangley@gmail.com>
2021-12-08 17:41:49 +0000

    Test and implement bingo_board_reader

--- day04.py
+++ day04.py
@@ -4,3 +4,24 @@ import numpy as np
 class BingoBoard:
     def __init__(self, size, values):
         self.board = np.fromiter(values, int, count=size * size).reshape(size, size)
+        self.board_called = np.ones_like(self.board, dtype=bool)
+
+
+def bingo_board_reader(values, board_size):
+    while True:
+        buffer = [next(values, None) for i in range(board_size * board_size)]
+        print(buffer)
+
+        # Consumption finished
+        if all(v == None for v in buffer):
+            break
+        # Consumption failed
+        if any(v == None for v in buffer):
+            raise ValueError("Insufficient values to build full board")
+
+        yield BingoBoard(board_size, buffer)
+
+
+class BingoGame:
+    def __init__(self, board_size, values):
+        self.boards = list(bingo_board_reader(values, board_size))
--- test_day04.py
+++ test_day04.py
@@ -2,7 +2,7 @@ import pytest
 
 import numpy as np
 
-from day04 import BingoBoard
+from day04 import BingoBoard, bingo_board_reader
 
 
 @pytest.mark.parametrize(
@@ -21,7 +21,58 @@ def test_BingoBoard_creation(size, values, exp):
     """
 
     # WHEN
-    bb = BingoBoard(size, iter(values))
+    bb = BingoBoard(size, values)
 
     # THEN
     assert np.array_equal(bb.board, exp)
+
+
+@pytest.mark.parametrize(
+    "size,values,exp",
+    [
+        (1, [], np.array([])),
+        (1, [1, 2, 3, 4], np.array([[[1]], [[2]], [[3]], [[4]]])),
+        (2, [1, 2, 3, 4, 5, 6, 7, 8], np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])),
+        (
+            3,
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+            np.array(
+                [
+                    [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                    [[11, 12, 13], [14, 15, 16], [17, 18, 19]],
+                ]
+            ),
+        ),
+    ],
+)
+def test_bingo_board_reader(size, values, exp):
+    # GIVEN
+    """
+    inputs: size, values
+    expected board: exp
+    """
+
+    # WHEN
+    boards = [b.board for b in bingo_board_reader(iter(values), size)]
+
+    # THEN
+    assert np.array_equal(np.array(boards), exp)
+
+
+@pytest.mark.parametrize(
+    "size,values",
+    [
+        (2, [1, 2, 3, 4, 5, 6, 7]),
+        (3, [1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 17, 18, 19]),
+    ],
+)
+def test_bingo_board_reader_invalid(size, values):
+    # GIVEN
+    """
+    inputs: size, values
+    """
+
+    # THEN
+    with pytest.raises(ValueError):
+        # WHEN
+        boards = [b.board for b in bingo_board_reader(iter(values), size)]
