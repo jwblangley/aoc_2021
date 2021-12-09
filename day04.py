4b995351830154ba17683913cfad116670277aaf
James Langley <jwblangley@gmail.com>
2021-12-09 16:30:50 +0000

    Implement day04 main part 1

--- day04.py
+++ day04.py
@@ -16,7 +16,6 @@ class BingoBoard:
 def bingo_board_reader(values, board_size):
     while True:
         buffer = [next(values, None) for i in range(board_size * board_size)]
-        print(buffer)
 
         # Consumption finished
         if all(v == None for v in buffer):
@@ -31,10 +30,12 @@ def bingo_board_reader(values, board_size):
 class BingoGame:
     def __init__(self, board_size, values):
         self.boards = list(bingo_board_reader(values, board_size))
+        self.last_call = None
 
     def call_number(self, number):
         for board in self.boards:
             board.call_number(number)
+        self.last_call = number
 
     def winners(self):
         return [b for b in self.boards if b.is_winner()]
@@ -46,3 +47,23 @@ class BingoGame:
             if len(winners) > 0:
                 return winners
         return []
+
+
+if __name__ == "__main__":
+    BOARD_SIZE = 5
+    with open("inputs/day04_input.txt", "r") as game_data:
+        calls = (int(c) for c in next(game_data).split(","))
+        board_values = (
+            v for values in game_data for v in values.strip().split(" ") if v != ""
+        )
+
+        bg = BingoGame(BOARD_SIZE, board_values)
+
+        winners = bg.play(calls)
+
+    assert len(winners) == 1
+
+    winning_board = winners[0]
+    score = winning_board.board[~winning_board.board_called].sum() * bg.last_call
+
+    print(f"Winning board score: {score}")
