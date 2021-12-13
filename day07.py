ec7eb2c723c3514b54f5d30c27452cac74198eec
James Langley <jwblangley@gmail.com>
2021-12-13 14:56:16 +0000

    Implement and test triangular number

--- day07.py
+++ day07.py
@@ -1,7 +1,21 @@
-import numpy as np
-
-
-def min_distance_meet_point(arr):
-    return min(arr) + np.argmin(
-        [sum(abs(x - t) for x in arr) for t in range(min(arr), max(arr))]
+def min_distance_fuel_cost(arr, burn_rate=lambda x: x):
+    return min(
+        sum(burn_rate(abs(x - t)) for x in arr) for t in range(min(arr), max(arr))
     )
+
+
+def triangular_number(x):
+    return x * (x + 1) / 2
+
+
+if __name__ == "__main__":
+    with open("inputs/day07_input.txt") as data:
+        line = next(data)
+
+    crabs = [int(v) for v in line.split(",")]
+
+    meet_point = min_distance_fuel_cost(crabs)
+    print(f"Meet point (linear burn rate): {meet_point}")
+
+    meet_point = min_distance_fuel_cost(crabs)
+    print(f"Meet point (linear burn rate): {meet_point}")
--- test_day07.py
+++ test_day07.py
@@ -1,10 +1,10 @@
 import pytest
 
-from day07 import min_distance_meet_point
+from day07 import min_distance_fuel_cost, triangular_number
 
 
-@pytest.mark.parametrize("arr, exp", [([16, 1, 2, 0, 4, 2, 7, 1, 2, 14], 2)])
-def test_min_distance_meet_point(arr, exp):
+@pytest.mark.parametrize("arr, exp", [([16, 1, 2, 0, 4, 2, 7, 1, 2, 14], 37)])
+def test_min_distance_fuel_cost(arr, exp):
     # GIVEN
     """
     input: arr
@@ -12,7 +12,37 @@ def test_min_distance_meet_point(arr, exp):
     """
 
     # WHEN
-    res = min_distance_meet_point(arr)
+    res = min_distance_fuel_cost(arr)
+
+    # THEN
+    assert res == exp
+
+
+@pytest.mark.parametrize(
+    "val,exp",
+    [
+        (1, 1),
+        (2, 3),
+        (3, 6),
+        (4, 10),
+        (5, 15),
+        (6, 21),
+        (7, 28),
+        (8, 36),
+        (9, 45),
+        (10, 55),
+        (999, 499500),
+    ],
+)
+def test_triangular_number(val, exp):
+    # GIVEN
+    """
+    input: val
+    expected value: exp
+    """
+
+    # WHEN
+    res = triangular_number(val)
 
     # THEN
     assert res == exp
