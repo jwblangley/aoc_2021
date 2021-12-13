3389ed05bb4d6822160c1f9dc425f379bf23e771
James Langley <jwblangley@gmail.com>
2021-12-13 23:32:26 +0000

    Test and implement day 8 part 2

--- day08.py
+++ day08.py
@@ -23,6 +23,114 @@ def count_match_length_patterns(it, match_lengths=UNIQUE_LENGTHS):
     return len([v for v in it if len(v) in match_lengths])
 
 
+def determine_mapping(signal_patterns):
+    len_signal_mapping = {
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+    }
+
+    number_mapping = dict()
+
+    for signal in signal_patterns:
+        len_signal_mapping[len(signal)].append(signal)
+
+    # 1
+    if len(len_signal_mapping[2]) != 1:
+        raise ValueError("Unable to determine mapping: 1")
+    number_mapping[1] = len_signal_mapping[2][0]
+
+    # 7
+    if len(len_signal_mapping[3]) != 1:
+        raise ValueError("Unable to determine mapping: 7")
+    number_mapping[7] = len_signal_mapping[3][0]
+
+    # 4
+    if len(len_signal_mapping[4]) != 1:
+        raise ValueError("Unable to determine mapping: 4")
+    number_mapping[4] = len_signal_mapping[4][0]
+
+    # 8
+    if len(len_signal_mapping[7]) != 1:
+        raise ValueError("Unable to determine mapping: 8")
+    number_mapping[8] = len_signal_mapping[7][0]
+
+    # 9
+    for signal in len_signal_mapping[6]:
+        if set(number_mapping[4]).issubset(set(signal)):
+            number_mapping[9] = signal
+            break
+    else:
+        raise ValueError("Unable to determine mapping: 9")
+
+    # 0
+    for signal in len_signal_mapping[6]:
+        if signal not in number_mapping.values() and set(number_mapping[1]).issubset(
+            set(signal)
+        ):
+            number_mapping[0] = signal
+            break
+    else:
+        raise ValueError("Unable to determine mapping: 6")
+
+    # 6
+    possible_signals = [
+        signal
+        for signal in len_signal_mapping[6]
+        if signal not in number_mapping.values()
+    ]
+    if len(possible_signals) != 1:
+        raise ValueError("Unable to determine mapping: 0")
+    number_mapping[6] = possible_signals[0]
+
+    # 3
+    for signal in len_signal_mapping[5]:
+        if set(number_mapping[1]).issubset(set(signal)):
+            number_mapping[3] = signal
+            break
+    else:
+        raise ValueError("Unable to determine mapping: 9")
+
+    # 5
+    for signal in len_signal_mapping[5]:
+        if signal not in number_mapping.values() and set(signal).issubset(
+            set(number_mapping[6])
+        ):
+            number_mapping[5] = signal
+            break
+    else:
+        raise ValueError("Unable to determine mapping: 5")
+
+    # 2
+    possible_signals = [
+        signal
+        for signal in len_signal_mapping[5]
+        if signal not in number_mapping.values()
+    ]
+    if len(possible_signals) != 1:
+        raise ValueError("Unable to determine mapping: 2")
+    number_mapping[2] = possible_signals[0]
+
+    return {"".join(sorted(v)): k for k, v in number_mapping.items()}
+
+
+def value_from_mapping(value, mapping):
+    acc = 0
+    for i, v in enumerate(reversed(value)):
+        acc += mapping["".join(sorted(v))] * 10 ** i
+    return acc
+
+
+def value_from_line(signal_patterns, output_value):
+    mapping = determine_mapping(signal_patterns)
+    value = value_from_mapping(output_value, mapping)
+    return value
+
+
 if __name__ == "__main__":
     with open("inputs/day08_input.txt", "r") as data:
         part1 = sum(
@@ -31,3 +139,8 @@ if __name__ == "__main__":
         )
 
     print(f"Part 1: {part1}")
+
+    with open("inputs/day08_input.txt", "r") as data:
+        part2 = sum(value_from_line(*parse_line(line)) for line in data)
+
+    print(f"Part 2: {part2}")
--- test_day08.py
+++ test_day08.py
@@ -1,6 +1,12 @@
 import pytest
 
-from day08 import count_match_length_patterns, parse_line
+from day08 import (
+    count_match_length_patterns,
+    determine_mapping,
+    parse_line,
+    value_from_line,
+    value_from_mapping,
+)
 
 
 @pytest.mark.parametrize(
@@ -65,6 +71,132 @@ def test_count_match_length_patterns(it, match_lengths, exp):
 
     # WHEN
     res = count_match_length_patterns(iter(it), match_lengths)
+
+    # THEN
+    assert res == exp
+
+
+@pytest.mark.parametrize(
+    # fmt: off
+    "signal_pattern,exp",
+    [
+        (
+            [ "acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"],
+            { "abcdefg": 8, "bcdef": 5, "acdfg": 2, "abcdf": 3, "abd": 7, "abcdef": 9, "bcdefg": 6, "abef": 4, "abcdeg": 0, "ab": 1},
+        ),
+    ],
+    # fmt: on
+)
+def test_determine_mapping(signal_pattern, exp):
+    # GIVEN
+    """
+    input: signal_pattern
+    expected value: exp
+    """
+
+    # WHEN
+    res = determine_mapping(signal_pattern)
+
+    # THEN
+    print(res)
+    print(exp)
+    assert res == exp
+
+
+@pytest.mark.parametrize(
+    "value,mapping,exp",
+    [
+        ("a", {"a": 3}, 3),
+        ("aa", {"a": 3}, 33),
+        ("aaa", {"a": 3}, 333),
+        ("abc", {"a": 3, "b": 2, "c": 1}, 321),
+    ],
+)
+def test_value_from_mapping(value, mapping, exp):
+    # GIVEN
+    """
+    input: value, mapping
+    expected value: exp
+    """
+
+    # WHEN
+    res = value_from_mapping(value, mapping)
+
+    # THEN
+    assert res == exp
+
+
+@pytest.mark.parametrize(
+    "signal_pattern,output_value,exp",
+    [
+        # fmt: off
+        (
+            ["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"],
+            ["cdfeb", "fcadb", "cdfeb", "cdbaf"],
+            5353
+        ),
+        (
+            ["be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb"],
+            ["fdgacbe", "cefdb", "cefbgd", "gcbe"],
+            8394
+        ),
+        (
+            ["edbfga", "begcd", "cbg", "gc", "gcadebf", "fbgde", "acbgfd", "abcde", "gfcbed", "gfec"],
+            ["fcgedb", "cgb", "dgebacf", "gc"],
+            9781
+        ),
+        (
+            ["fgaebd", "cg", "bdaec", "gdafb", "agbcfd", "gdcbef", "bgcad", "gfac", "gcb", "cdgabef"],
+            ["cg", "cg", "fdcagb", "cbg"],
+            1197
+        ),
+        (
+            ["fbegcd", "cbd", "adcefb", "dageb", "afcb", "bc", "aefdc", "ecdab", "fgdeca", "fcdbega"],
+            ["efabcd", "cedba", "gadfec", "cb"],
+            9361
+        ),
+        (
+            ["aecbfdg", "fbg", "gf", "bafeg", "dbefa", "fcge", "gcbea", "fcaegb", "dgceab", "fcbdga"],
+            ["gecf", "egdcabf", "bgf", "bfgea"],
+            4873
+        ),
+        (
+            ["fgeab", "ca", "afcebg", "bdacfeg", "cfaedg", "gcfdb", "baec", "bfadeg", "bafgc", "acf"],
+            ["gebdcfa", "ecba", "ca", "fadegcb"],
+            8418
+        ),
+        (
+            ["dbcfg", "fgd", "bdegcaf", "fgec", "aegbdf", "ecdfab", "fbedc", "dacgb", "gdcebf", "gf"],
+            ["cefg", "dcbef", "fcge", "gbcadfe"],
+            4548
+        ),
+        (
+            ["bdfegc", "cbegaf", "gecbf", "dfcage", "bdacg", "ed", "bedf", "ced", "adcbefg", "gebcd"],
+            ["ed", "bcgafe", "cdgba", "cbgef"],
+            1625
+        ),
+        (
+            ["egadfb", "cdbfeg", "cegd", "fecab", "cgb", "gbdefca", "cg", "fgcdab", "egfdb", "bfceg"],
+            ["gbdfcae", "bgc", "cg", "cgb"],
+            8717
+        ),
+        (
+            ["gcafb", "gcf", "dcaebfg", "ecagb", "gf", "abcdeg", "gaef", "cafbge", "fdbac", "fegbdc"],
+            ["fgae", "cfgab", "fg", "bagce"],
+            4315
+        ),
+        # fmt: on
+    ],
+)
+def test_seven_segment_integration(signal_pattern, output_value, exp):
+    # GIVEN
+    """
+    input: signal_patter, output_value
+    expected value: exp
+    """
+
+    # WHEN
+    res = value_from_line(signal_pattern, output_value)
 
     # THEN
     assert res == exp
