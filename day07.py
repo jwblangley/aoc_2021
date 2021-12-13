cdb504957a12919fe747a3cf1b6ea2bc8b149128
James Langley <jwblangley@gmail.com>
2021-12-13 14:40:21 +0000

    Implement min distance meet point

--- day07.py
+++ day07.py
@@ -1,2 +1,7 @@
+import numpy as np
+
+
 def min_distance_meet_point(arr):
-    return None
+    return min(arr) + np.argmin(
+        [sum(abs(x - t) for x in arr) for t in range(min(arr), max(arr))]
+    )
