551eff5fadec5914f791c54a1aa6d649090dd819
James Langley <jwblangley@gmail.com>
2021-12-21 21:34:49 +0000

    Write day09 part 1 main

--- day09.py
+++ day09.py
@@ -10,3 +10,12 @@ def get_low_points(grid):
     xn = np.roll(pad, -1, 1)[1:-1, 1:-1]
 
     return (grid < yp) & (grid < yn) & (grid < xp) & (grid < xn)
+
+
+if __name__ == "__main__":
+    with open("inputs/day09_input.txt", "r") as data:
+        grid = np.array([[int(c) for c in line.strip()] for line in data])
+
+    low_points = get_low_points(grid)
+    total_risk = (1 + grid)[low_points].sum()
+    print(f"Total risk: {total_risk}")
