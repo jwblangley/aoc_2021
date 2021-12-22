5288af7e8b935a7878519336edf372cbdad8c2e5
James Langley <jwblangley@gmail.com>
2021-12-22 12:33:09 +0000

    Test and implement num_flows_to

--- day09.py
+++ day09.py
@@ -12,6 +12,32 @@ def get_low_points(grid):
     return (grid < yp) & (grid < yn) & (grid < xp) & (grid < xn)
 
 
+def num_flows_to(grid, i, j, prev=0, seen=None):
+    if seen is None:
+        seen = set()
+
+    if i < 0 or i >= grid.shape[0] or j < 0 or j >= grid.shape[1]:
+        return 0
+
+    if (i, j) in seen:
+        return 0
+
+    if grid[i, j] >= 9:
+        return 0
+
+    if grid[i, j] >= prev:
+        seen.add((i, j))
+        return (
+            1
+            + num_flows_to(grid, i - 1, j, grid[i, j], seen)
+            + num_flows_to(grid, i + 1, j, grid[i, j], seen)
+            + num_flows_to(grid, i, j - 1, grid[i, j], seen)
+            + num_flows_to(grid, i, j + 1, grid[i, j], seen)
+        )
+
+    return 0
+
+
 if __name__ == "__main__":
     with open("inputs/day09_input.txt", "r") as data:
         grid = np.array([[int(c) for c in line.strip()] for line in data])
