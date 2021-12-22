1bd21ffec896b051f059f8f0e7d5569c3c6ad7e4
James Langley <jwblangley@gmail.com>
2021-12-22 13:37:20 +0000

    Change interface to return stack

--- day10.py
+++ day10.py
@@ -10,7 +10,6 @@ score_map = {
     "]": 57,
     "}": 1197,
     ">": 25137,
-    None: 0,
 }
 
 
@@ -22,11 +21,15 @@ def is_corrupt_parenthesis(string):
         if c in parantheses.values():
             if len(stack) == 0 or c != parantheses[stack.pop()]:
                 return c
-    return None
+    return stack
 
 
 if __name__ == "__main__":
     with open("inputs/day10_input.txt", "r") as data:
-        total_score = sum(score_map[is_corrupt_parenthesis(line)] for line in data)
+        total_score = sum(
+            score_map[is_corrupt_parenthesis(line)]
+            for line in data
+            if not isinstance(is_corrupt_parenthesis(line), list)
+        )
 
     print(f"Total score: {total_score}")
