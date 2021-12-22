c45d468545e818e2fc77c87bb5851b947dff9e59
James Langley <jwblangley@gmail.com>
2021-12-22 13:40:42 +0000

    Add tests for incomplete parantheses

--- day10.py
+++ day10.py
@@ -17,9 +17,9 @@ def is_corrupt_parenthesis(string):
     stack = []
     for c in string:
         if c in parantheses.keys():
-            stack.append(c)
+            stack.append(parantheses[c])
         if c in parantheses.values():
-            if len(stack) == 0 or c != parantheses[stack.pop()]:
+            if len(stack) == 0 or c != stack.pop():
                 return c
     return stack
 
