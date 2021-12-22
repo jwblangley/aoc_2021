25f9b679b18d3b6f6f34acee4531d00558c8b1e4
James Langley <jwblangley@gmail.com>
2021-12-22 13:27:31 +0000

    Implement day10 part 1 main

--- day10.py
+++ day10.py
@@ -5,6 +5,14 @@ parantheses = {
     "<": ">",
 }
 
+score_map = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+    None: 0,
+}
+
 
 def is_corrupt_parenthesis(string):
     stack = []
@@ -14,5 +22,11 @@ def is_corrupt_parenthesis(string):
         if c in parantheses.values():
             if len(stack) == 0 or c != parantheses[stack.pop()]:
                 return c
-
     return None
+
+
+if __name__ == "__main__":
+    with open("inputs/day10_input.txt", "r") as data:
+        total_score = sum(score_map[is_corrupt_parenthesis(line)] for line in data)
+
+    print(f"Total score: {total_score}")
