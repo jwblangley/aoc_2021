53281d3caca029d88f5088dd73b9cf6ade5ae117
James Langley <jwblangley@gmail.com>
2021-12-23 14:01:16 +0000

    Implement day12 part 2

--- day12.py
+++ day12.py
@@ -12,7 +12,11 @@ class Cave:
 
 
 def count_paths_to_end(
-    start_cave, path=None, end_cave_uid="end", single_small_cave_double_visit=False
+    start_cave,
+    path=None,
+    end_cave_uid="end",
+    single_small_cave_double_visit=False,
+    has_double_visit=False,
 ):
     # N.B: Does not detect infinite loops
     if path is None:
@@ -21,11 +25,30 @@ def count_paths_to_end(
     if start_cave.uid == end_cave_uid:
         return 1
 
-    if not start_cave.is_big_cave() and start_cave.uid in path:
-        return 0
+    if single_small_cave_double_visit:
+        if has_double_visit and not start_cave.is_big_cave() and start_cave.uid in path:
+            return 0
+    else:
+        if not start_cave.is_big_cave() and start_cave.uid in path:
+            return 0
+
+    if (
+        single_small_cave_double_visit
+        and not start_cave.is_big_cave()
+        and start_cave.uid in path
+    ):
+        if start_cave.uid == "start":
+            return 0
+        has_double_visit = True
 
     return sum(
-        count_paths_to_end(nb, path=[*path, start_cave.uid], end_cave_uid=end_cave_uid)
+        count_paths_to_end(
+            nb,
+            path=[*path, start_cave.uid],
+            end_cave_uid=end_cave_uid,
+            single_small_cave_double_visit=single_small_cave_double_visit,
+            has_double_visit=has_double_visit,
+        )
         for nb in start_cave.neighbours
     )
 
